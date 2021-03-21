aeb9aaa9af9bf3c58fe40a95d501ba2dfe26113a
Eszter Schuffert <schufferte@yahoo.com>
2021-03-21 11:46:36 +0100

    add supplier data

--- odata/pages/suppliers.py
+++ odata/pages/suppliers.py
@@ -2,19 +2,13 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import marketing
+import suppliers
 
 suppliers_layout = html.Div([
     html.H2(children='Suppliers'),
     dcc.Graph(
-        id='marketing-area',
-        figure=marketing.marketingVisualization.getFigureByArea()
+        id='suppliersPerTime',
+        figure=suppliers.supplierVisualization.getFigure()
     ),
-    
-    dcc.Graph(
-        id='marketing-area',
-        figure=marketing.marketingVisualization.getFigureByTime("")
-    ),
-    
     html.Div(id='suppliers-content')
 ])
---
+++ odata/suppliers.py
@@ -0,0 +1,44 @@
+#odata
+import requests
+import pyodata
+import pandas as pd
+
+#classes
+from service import service
+
+class supplierData:
+    
+    def fetch():
+        materials = []
+        dates = []
+        prices = []
+        vendors = []
+    
+        p = service.theservice.entity_sets.Suppliers_Prices.get_entities()
+        for p_ in p.execute():
+            material = p_.MATERIAL_DESCRIPTION
+            date = p_.SIM_DATE
+            date = service.formatDate(date)
+            price = p_.NET_PRICE
+            vendor = p_.VENDOR_NAME
+            
+            materials.append(material)
+            dates.append(date)
+            prices.append(price)
+            vendors.append(vendor)
+        return [materials, dates, prices, vendors]
+
+# Visualization
+import plotly.express as px
+
+class supplierVisualization:
+    def getFigure():
+        df = pd.DataFrame({
+            "Material":supplierData.fetch()[0],
+            "Date":supplierData.fetch()[1],
+            "Price":supplierData.fetch()[2],
+            "Vendor":supplierData.fetch()[3],
+        })
+
+        fig = px.line(df, x="Date", y="Price", color="Material", hover_data=["Material", "Price", "Vendor"])
+        return fig
