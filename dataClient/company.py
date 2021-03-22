f36559470f408ecbb8db08c5835675b3577a76d0
Eszter Schuffert <schufferte@yahoo.com>
2021-03-22 16:58:03 +0100

    add financial data

--- dataClient/company.py
+++ dataClient/company.py
@@ -9,30 +9,77 @@ from dataClient.helper import helper as h
 
 class companyData:
     
-    def fetch():
-        valuation = []
+    def fetchCompanyValuation():
         dates = []
+        
+        cashHistory = []
+        accReceivables = []
+        loanHistory = []
+        accPayables = []
+        profits = []
+        
+        debtLoadings = []
+        creditRatings = []
+        valuation = []
     
         p = service.theservice.entity_sets.Company_Valuation.get_entities()
-        for p_ in p.execute():
-            val = p_.COMPANY_VALUATION
+        for p_ in p.execute():  
             date = p_.SIM_DATE
             date = h.formatDate(date)
             
+            cash = p_.BANK_CASH_ACCOUNT
+            accReceivable = p_.ACCOUNTS_RECEIVABLE
+            loan = p_.BANK_LOAN
+            accPayable = p_.ACCOUNTS_PAYABLE
+            profit = p_.PROFIT
             
-            valuation.append(float(val))
+            debtLoading = p_.DEBT_LOADING
+            creditRating = p_.CREDIT_RATING
+            val = p_.COMPANY_VALUATION
+            
             dates.append(date)
-        return [valuation, dates]
+            
+            cashHistory.append(float(cash))
+            accReceivables.append(float(accReceivable))
+            loanHistory.append(float(loan))
+            accPayables.append(float(accPayable))
+            profits.append(float(profit))
+            
+            debtLoadings.append(float(debtLoading))
+            creditRatings.append(creditRating)
+            valuation.append(float(val))
+            
+        return [dates, cashHistory, accReceivables, loanHistory, accPayables, profits, debtLoadings, creditRatings, valuation]
 
 # Visualization
 import plotly.express as px
 
 class companyVisualization:
-    def getFigure():
+    def getCompanyValuationFigure():
         df = pd.DataFrame({
-            "Company Valuation": companyData.fetch()[0],
-            "Date": companyData.fetch()[1]
+            "Company Valuation": companyData.fetchCompanyValuation()[8],
+            "Date": companyData.fetchCompanyValuation()[0],
+            "Credit Rating": companyData.fetchCompanyValuation()[7]
         })
 
-        fig = px.line(df, x="Date", y="Company Valuation")
+        fig = px.line(df, x="Date", y="Company Valuation", hover_data=["Credit Rating"])
+        return fig
+    
+    def getDebtLoadingProfitsFigure():
+        df = pd.DataFrame({
+            "Debt Loading": companyData.fetchCompanyValuation()[6],
+            "Profit": companyData.fetchCompanyValuation()[5],
+            "Cash": companyData.fetchCompanyValuation()[1],
+            "Account Receivable": companyData.fetchCompanyValuation()[2],
+            "Loans": companyData.fetchCompanyValuation()[3],
+            "Account Payables": companyData.fetchCompanyValuation()[4],
+            "Credit Rating": companyData.fetchCompanyValuation()[7],
+            "Date": companyData.fetchCompanyValuation()[0]
+        })
+
+        fig = px.line(df, x="Date", 
+                      y=["Debt Loading", "Profit", "Cash", "Account Receivable", "Loans", "Account Payables"],
+                      hover_data=["Credit Rating"],
+                      labels={"value": "Amount",
+                        "variable": "KPI"})
         return fig
--- pages/company.py
+++ pages/company.py
@@ -6,9 +6,15 @@ from dataClient.company import companyVisualization as vis
 
 company_layout = html.Div([
     html.H2(children='Company'),
+    html.H3(children='Company Valuation'),
     dcc.Graph(
         id='company-valuation',
-        figure=vis.getFigure()
+        figure=vis.getCompanyValuationFigure()
+    ),
+    html.H3(children='Debt Loading & Profits'),
+    dcc.Graph(
+        id='company-debt-loading-profits',
+        figure=vis.getDebtLoadingProfitsFigure()
     ),
     dcc.Interval(
         id='interval-component',
