2c6ba84d97c8af20fe1ff2400a64a4e49c07454b
Eszter Schuffert <schufferte@yahoo.com>
2021-03-22 18:45:53 +0100

    hide secrets

--- dataClient/service.py
+++ dataClient/service.py
@@ -1,11 +1,12 @@
+from decouple import config
 # odata 
 import requests
 import pyodata
 from pyodata.v2.model import PolicyFatal, PolicyWarning, PolicyIgnore, ParserError, Config
 
 class service:
-    SERVICE_URL = 'https://r73z.ucc.ovgu.de:/odata/700/'
+    SERVICE_URL = config("SERVICEURL")
 
     session = requests.Session()
-    session.auth = ('C_3', 'etc-oxide-GARBAGE')
+    session.auth = (config("USERNAME"), config("PASSWORD"))
     theservice = pyodata.Client(SERVICE_URL, session)
